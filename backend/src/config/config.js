c79dfa7412363fecdcd1c8870c7d6cdba9aef289
Harshit joshi <harshjoshi25022002@gmail.com>
2026-07-13 17:06:23 +0530

    feat: add passport and Google OAuth integration with environment variable checks

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -4,9 +4,16 @@ dotenv.config();
 if(!process.env.MONGO_URI){
   throw new Error("MONGO_URI is not defined in environment variable")
 }
+if(!process.env.GOOGLE_CLIENT_ID){
+  throw new Error("GOOGLE_CLIENT_ID is not defined in environment variables")
+}
 
 export default {
   port: process.env.PORT || 3000,
   mongoUri: process.env.MONGO_URI,
   jwtSecret: process.env.JWT_SECRET,
+  GOOGLE_CLIENT_ID:GOOGLE_CLIENT_ID,
+  GOOGLE_CLIENT_SECRET:GOOGLE_CLIENT_SECRET,
+  GOOGLE_CALLBACK_URL:GOOGLE_CALLBACK_URL
+
 };
