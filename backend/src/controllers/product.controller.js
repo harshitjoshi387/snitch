ef0f9ed3eea3b4e4493ccc91caa7a58bf8824350
Harshit joshi <harshjoshi25022002@gmail.com>
2026-07-15 15:45:03 +0530

    feat: implement createProduct function to handle product creation with image uploads

--- backend/src/controllers/product.controller.js
+++ backend/src/controllers/product.controller.js
@@ -2,5 +2,29 @@ import{ productModel} from "../models/product.model.js"
 
 
 export async function createProduct(req,res) {
-    
+    const {title,description,price}=req.body
+    const seller= req.user
+    const images =await Promise.all(req.files.map(async(file)=>{
+        return await uploadFile({
+            buffer:file.buffer,
+            fileName:file.originalname
+        })
+    }))
+    const product= await productModel.create({
+        title,
+        description,
+        price:{
+            amount:price,
+            currency:"INR"
+        },
+        images,
+        seller:seller._id
+
+    })
+
+    res.status(201).json({
+        message:"product created successfully",
+        success:true,
+        product
+    })
 }
