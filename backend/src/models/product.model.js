028b112553ff15243ee3abb312fd5cf46551be4d
Harshit joshi <harshjoshi25022002@gmail.com>
2026-07-15 12:20:01 +0530

    in this commit  we created product schema

---
+++ backend/src/models/product.model.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose")
+
+const productSchema= new mongoose.Schema({
+    title:{
+        type:String,
+        required:true
+    },
+    description:{
+        type:String,
+        required:true
+    },
+    seller:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'user',
+        required:true
+    },
+    price:{
+        amount:{
+            type:String,
+            required:True
+        },
+        currency:{
+            type:String,
+            enum:["USD","EUR","GBP","JPY","INR"],
+            default:"INR"
+        }
+    },
+    images:[
+        {
+            url:{
+                type:String,
+                required:true
+            },
+            alt:{
+                type:String,
+                required:true
+            }
+        }
+    ]
+})
