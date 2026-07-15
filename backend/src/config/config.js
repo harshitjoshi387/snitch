514d3b8f8c57c2df238da644ee22b0ee6290549b
Harshit joshi <harshjoshi25022002@gmail.com>
2026-07-15 14:21:19 +0530

    feat: integrate ImageKit for file uploads and add multer for image handling

--- backend/src/config/config.js
+++ backend/src/config/config.js
@@ -7,6 +7,9 @@ if(!process.env.MONGO_URI){
 if(!process.env.GOOGLE_CLIENT_ID){
   throw new Error("GOOGLE_CLIENT_ID is not defined in environment variables")
 }
+if(!process.env.IMAGEKIT_PRIVATE_KEY){
+  throw new error("imgaekit private key is not define")
+}
 
 export default {
   port: process.env.PORT || 3000,
@@ -14,5 +17,6 @@ export default {
   jwtSecret: process.env.JWT_SECRET,
   GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
   GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
-  GOOGLE_CALLBACK_URL: process.env.GOOGLE_CALLBACK_URL
+  GOOGLE_CALLBACK_URL: process.env.GOOGLE_CALLBACK_URL,
+  IMAGEKIT_PRIVATE_KEY:process.env.IMAGEKIT_PRIVATE_KEY
 };
--- backend/src/routes/product.routes.js
+++ backend/src/routes/product.routes.js
@@ -1,10 +1,17 @@
 import{Router} from 'express'
 import {authenticateSeller}from "../middleware/auth.middleware.js"
 import { createProduct } from '../controllers/product.controller.js';
+import multer from "multer"
 const router = express.Router()
 
+const upload= multer({
+    storage:multer.memoryStorage(),
+    limits:{
+        fileSize:5*1024*1024
+    }
+})
 
-router.post("/product",authenticateSeller,createProduct)
+router.post("/product",authenticateSeller,upload.array("images",7),createProduct)
 
 
 
