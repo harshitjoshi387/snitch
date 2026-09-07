3422c5d836cba4c7d28c6e656ff4af12e7b3990f
harshitjoshi387 <harshjoshi25022002@gmail.com>
2026-09-07 19:55:22 +0530

    feat: initialize product slice with sellerProducts state and setSellerProducts reducer

---
+++ frontend/src/features/products/state/product.slice.js
@@ -0,0 +1,19 @@
+import {createSlice} from "@reduxjs/toolkit"
+
+const initialState ={
+  sellerProducts:[],
+}
+
+
+export const productSlice = createSlice({
+  name:"product",
+  initialState,
+  reducers:{
+    setSellerProducts:(state,action)=>{
+      state.sellerProducts=action.payload
+    }
+  }
+})
+
+export const {setSellerProducts}=productSlice.actions
+export default productSlice.reducer
